f0077ba690e37905be419fcef6b8fe09161ad549
Philip Kalela <philipkalela@Philips-MacBook-Pro.local>
2018-07-30 11:04:06 +0300

    [feature] set up link between home page and registration

--- app/src/main/java/com/andela/philskiiiwalker/brighteventsandroidapp/home/MainActivity.java
+++ app/src/main/java/com/andela/philskiiiwalker/brighteventsandroidapp/home/MainActivity.java
@@ -1,15 +1,30 @@
 package com.andela.philskiiiwalker.brighteventsandroidapp.home;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.andela.philskiiiwalker.brighteventsandroidapp.R;
+import com.andela.philskiiiwalker.brighteventsandroidapp.registration.SignUpActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button getStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getStarted = findViewById(R.id.signUpButton);
+
+        getStarted.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
