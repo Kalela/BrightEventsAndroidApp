4022ddf4c332acf2f38e8703ef00b892bc47839a
Kalela <philipkalela@gmail.com>
2018-11-07 11:52:44 +0300

    feature(app center): set up app center dependencies

--- app/src/main/java/com/andela/philskiiiwalker/brighteventsandroidapp/home/MainActivity.java
+++ app/src/main/java/com/andela/philskiiiwalker/brighteventsandroidapp/home/MainActivity.java
@@ -3,6 +3,7 @@ package com.andela.philskiiiwalker.brighteventsandroidapp.home;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.andela.philskiiiwalker.brighteventsandroidapp.R;
 
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("check", "onCreate: I am here");
         setContentView(R.layout.activity_main);
+
     }
 }
--- app/src/main/java/com/andela/philskiiiwalker/brighteventsandroidapp/login/LoginActivity.java
+++ app/src/main/java/com/andela/philskiiiwalker/brighteventsandroidapp/login/LoginActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.andela.philskiiiwalker.brighteventsandroidapp.R;
 import com.andela.philskiiiwalker.brighteventsandroidapp.registration.view.SignUpActivity;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class LoginActivity extends AppCompatActivity {
     TextView gotToSignUpLink;
@@ -18,6 +22,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        AppCenter.start(getApplication(),
+                "decf03ad-d782-4d7c-bc02-9c2e7ace780f",
+                Analytics.class, Crashes.class);
 
         gotToSignUpLink = findViewById(R.id.signInToSignUpLinkTextView);
         gotToSignUpLink.setOnClickListener(new Button.OnClickListener(){
