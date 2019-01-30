34c74e147cb6f279c93c5a0cc59cc28df9fae42e
Kalela <philipkalela@gmail.com>
2019-01-30 17:39:58 +0300

    feature(register): Register user functionality

--- app/src/main/java/com/andela/philskiiiwalker/brighteventsandroidapp/registration/presenter/RegisterPresenter.java
+++ app/src/main/java/com/andela/philskiiiwalker/brighteventsandroidapp/registration/presenter/RegisterPresenter.java
@@ -12,6 +12,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class RegisterPresenter {
+  
     RegisterContract.RegisterView view;
     private final AuthService authService;
 
